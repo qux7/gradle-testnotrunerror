fbc1d3002348497f7380d1520e58fea47af9abc2
Your Name <you@example.com>
2024-08-27 21:43:40 +0300

    add javadoc

--- build-with-gradle-6.2.1/src/main/groovy/io/github/qux7/testnotrunerror/TestNotRunErrorPluginExtension.groovy
+++ build-with-gradle-6.2.1/src/main/groovy/io/github/qux7/testnotrunerror/TestNotRunErrorPluginExtension.groovy
@@ -13,6 +13,12 @@ class TestNotRunErrorPluginExtension {
 
     static final List booleanFields = ['stopOnFailure', 'checkClasses', 'checkJavaSources', 'readSourceFiles', 'enabled'].asImmutable()
 
+    /**
+     * Given a project, examine its properties and set this object's fields according
+     * to the relevant project properties.
+     * @param theProject Gradle project
+     * @return this
+     */
     def setFromProjectProperties(Project theProject) {
         def projectProperties = theProject.properties
         def wasMnemonic = setMnemonicFromMap(projectProperties) { key, value ->
@@ -25,8 +31,17 @@ class TestNotRunErrorPluginExtension {
             println("WARNING: both checkJavaSources and checkClasses were disabled, setting them both to true")
             checkClasses = checkJavaSources = true
         }
+        return this
     }
 
+    /**
+     * Set the boolean fields of this object from the project properties map.
+     * For each boolean field {@code <foo>}, the corresponding property name is {@code testnotrunerror.<foo>}.
+     * @param map contains all project properties
+     * @param reportProblem a closure taking two arguments, key and value, invoked if the value specified
+     *                      for a boolean field is not 'true' or 'false'
+     * @return this
+     */
     TestNotRunErrorPluginExtension setFieldsFromMap(Map<String, String> map, Closure reportProblem) {
         booleanFields.each {
             def key = 'testnotrunerror.' + it
@@ -42,6 +57,16 @@ class TestNotRunErrorPluginExtension {
         return this
     }
 
+    /**
+     * If the passed map contains a "mnemonic override", that is,
+     * the property {@code 'test.not.run'} with the value of either {@code 'error'}, {@code 'warning'},
+     * or {@code 'ignore'}, set the fields {@code enabled} and {@code stopOnFailure} correspondingly.
+     * It is called "mnemonic" because it looks like "test.not.run=error", easy to remember.
+     * @param map contains all project properties
+     * @param reportProblem a closure taking two arguments, key and value, invoked if the value specified
+     *                      for a boolean field is not 'error', 'warning' or 'ignore'
+     * @return true if there was a "mnemonic override" in the map
+     */
     boolean setMnemonicFromMap(Map<String, String> map, Closure reportProblem) {
         def modified = false
         def key = 'test.not.run'
